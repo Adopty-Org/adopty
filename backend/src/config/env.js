a2913083f15f36edf3932e9027cec8485c77f8bc
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-03-12 16:12:20 +0100

    clerk truc plus dep-test

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,5 +5,12 @@ dotenv.config();
 export const ENV = {
     NODE_ENV:process.env.NODE_ENV,
     PORT:process.env.PORT,
-    DB_URL:process.env.DB_URL
+    DB_URL:process.env.DB_URL,
+    DB_HOST:process.env.DB_HOST,
+    DB_PORT:process.env.DB_PORT,
+    DB_USER:process.env.DB_USER,
+    DB_PASS:process.env.DB_PASS,
+    DB_NAME:process.env.DB_NAME,
+    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,18 @@
 import express from "express"
 import path from "path"
+import { clerkMiddleware } from '@clerk/express'
+
 import { ENV } from "./config/env.js"
+import { connectDB } from "./config/db.js"
 
 
 const app = express()
 
+
+
 const __dirname = path.resolve()
+
+app.use(clerkMiddleware())
 
 app.get("/api/calling", (req,res)=>{
     res.status(200).json({message: "oui ca fonctionne"})
@@ -20,4 +27,5 @@ if(ENV.NODE_ENV == "production"){
 
 app.listen(ENV.PORT, () => {
     console.log("le serveur roule ma boule")
+    connectDB()
 })
