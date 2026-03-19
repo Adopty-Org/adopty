1fb4f96b7fb549d32bf14af2469e25eff74cc083
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-03-19 22:15:53 +0800

    fix

---
+++ backend/src/modeles/utilisateur.model.js
@@ -0,0 +1,50 @@
+export class Utilisateur {
+    /*constructor({
+        Id,
+        clerkId,
+        stripeCustomerId,
+        Nom,
+        Prenom,
+        Addresse,
+        AddresseEmail,
+        Wilaya,
+        MotDePasse,
+        Photo,
+        CreeLe,
+        CreePar,
+        ModifieeLe,
+        ModifieePar
+    } = {}) {
+        this.Id = Id;
+        this.clerkId = clerkId;
+        this.stripeCustomerId = stripeCustomerId;
+        this.Nom = Nom;
+        this.Prenom = Prenom;
+        this.Addresse = Addresse;
+        this.AddresseEmail = AddresseEmail;
+        this.Wilaya = Wilaya;
+        this.MotDePasse = MotDePasse;
+        this.Photo = Photo;
+        this.CreeLe = CreeLe;
+        this.CreePar = CreePar;
+        this.ModifieeLe = ModifieeLe;
+        this.ModifieePar = ModifieePar;
+    };*/
+
+    constructor(data) {
+    this.Id = data.Id
+    this.clerkId = data.clerkId;
+    this.stripeCustomerId = data.stripeCustomerId;
+    this.Nom = data.Nom
+    this.Prenom = data.Prenom
+    this.Addresse = data.Addresse
+    this.AddresseEmail = data.AddresseEmail;
+    this.Wilaya = data.Wilaya;
+    this.MotDePasse = data.MotDePasse;
+    this.Photo = data.Photo;
+    this.CreeLe = data.CreeLe;
+    this.CreePar = data.CreePar;
+    this.ModifieeLe = data.ModifieeLe;
+    this.ModifieePar = data.ModifieePar;
+  }
+}
