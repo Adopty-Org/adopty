86b4263f3abc5774cbdde59509b6c19dd2be84e4
Vector2747 <zakaria.tafoukt@se.univ-bejaia.dz>
2026-03-26 15:57:03 +0800

    routes web (squelette) et autres choses minimes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
-import {  SignInButton, SignUpButton, UserButton } from '@clerk/clerk-react'
+import {  SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/clerk-react'
+import { Navigate, Route, Routes }from "react-router"
+import LoginPage from './pages/LoginPage'
+import LobbyPage from './pages/LobbyPage'
+import ShopPage from './pages/ShopPage'
+import ServicesPage from './pages/ServicesPage'
+import RefugesNAnimals from './pages/RefugesNAnimalsPage'
+import ConserningPage from './pages/ConserningPage'
+import LobbyLayout from './layouts/LobbyLayout'
+import LoadingLayout from './components/Loadingpage'
+
 
 
 function App() {
+
+  const { isSignedIn,isLoaded } = useAuth()
+
+  if(!isLoaded) return <LoadingLayout/>;
+
   return (
-    <div>
-      <header>
-        <button className="btn btn-primary">Test</button>
-          <SignInButton mode='modal'/>
-          <SignUpButton mode='modal'/>
-        
-        
-          <UserButton />
-        
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" element={ isSignedIn ? <Navigate to = {"/lobby"}/> : <LoginPage/> }/>
+
+      <Route path="/" element={<LobbyLayout/>}>  /* pas de isSignedIn car debile */
+        <Route index element={<Navigate to={"LobbyPage"}/>}/>
+        <Route path="lobby" element={<LobbyPage/>}/>
+        <Route path="shop"element={<ShopPage/>}/>
+        <Route path="services"element={<ServicesPage/>}/>
+        <Route path="refanimal"element={<RefugesNAnimals/>}/>
+        <Route path="conserning"element={<ConserningPage/>}/>
+
+      </Route>
+    </Routes>
   )
 }
 
---
+++ web/src/pages/ConserningPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ConserningPage() {
+  return (
+    <div>ConserningPage</div>
+  )
+}
+
+export default ConserningPage
